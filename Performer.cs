a3a654c5f291d687119b81042175f1e67a07ef1d
cat32tw <mike1993xd@gmail.com>
2022-02-13 17:27:53 +0800

    靜態成員練習

---
+++ Performer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_member
+{
+    internal class Performer
+    {
+        public string Name { get; set; }
+        private static int num;
+        public static int Num
+        {
+            get { return num; }
+        }
+        public Performer()
+        {
+            num++;
+        }
+        public Performer(string vname)
+        {
+            Name = vname;
+            num++;
+        }
+        public static void ShowNum()
+        {
+            Console.WriteLine("目前出場總人數: {0} 人", Num);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace static_member
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Performer person1 = new Performer();
+            person1.Name = "陸一中";
+            Console.WriteLine("姓名:{0}", person1.Name);
+            Console.WriteLine("第 {0} 位表演者", Performer.Num);
+            Console.WriteLine("------------------------------");
+            Performer person2 = new Performer("蔡竹男");
+            Console.WriteLine("姓名:{0}", person2.Name);
+            Console.WriteLine("第 {0} 位表演者", Performer.Num);
+            Console.WriteLine("==============================");
+            Performer.ShowNum();
+            Console.Read();
+        }
+    }
+}
